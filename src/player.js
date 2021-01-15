ac27e1acd1f7c74410d5ad31dce72b1b8bdeb398
JS <jeff.shep777@gmail.com>
2021-01-14 19:09:25 -0700

    Add functionality to player methods where possible

--- src/player.js
+++ src/player.js
@@ -1,14 +1,20 @@
-
 class Player {
   constructor(id) {
     this.id = id;
     this.wins = 0;
     this.hand = [];
   }
-  playCard() {
 
+  playCard() {
+    if (this.hand.length !== 0) {
+      //add card to central pile
+    } else {
+      //show a message saying you have no cards to play?
+    }
   }
+
   saveWinsToStorage() {
-    
+    var wins = JSON.stringify(this.wins);
+    localStorage.setItem('playerOne', wins);
   }
 }
