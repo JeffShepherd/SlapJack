7fa26aaa78cb5ad67f50220c42e79efd64a44535
JS <jeff.shep777@gmail.com>
2021-01-16 15:38:10 -0700

    Break down checkKeyPress into more specific functions

--- src/main.js
+++ src/main.js
@@ -15,24 +15,33 @@ document.addEventListener('keydown', function(event) {
 
 function checkKeyPress(event) {
   var key = event.keyCode;
+  checkForSlapKey(key);
+  checkForDealKey(key);
+}
+
+function checkForSlapKey(key) {
   if (key === 70) { //key f - playerOne slap
     if (game.slap()) {
-      console.log('shuffle')//take cards and shuffle them into deck
+      console.log('shuffle') //take middle cards and shuffle them into deck
     } else {
-      game.punishBadSlap(1);//bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
+      game.punishBadSlap(1); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
     }
   } else if (key === 74) { //key j - playerTwo slap
     if (game.slap()) {
-      console.log('shuffle')//take cards and shuffle them into deck
+      console.log('shuffle') //take middle cards and shuffle them into deck
     } else {
-      game.punishBadSlap(2);//bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
+      game.punishBadSlap(2); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
     }
-  } else if (key === 81 && game.playerTurn === 1 && game.playerOne.hand.length !== 0) { //key q - playerOne deal if it's their turn
-    game.moveCardToMiddle();//move playerOne top card to middle
-    game.playerTurn = 2;//change player turn
+  }
+}
+
+function checkForDealKey(key) {
+  if (key === 81 && game.playerTurn === 1 && game.playerOne.hand.length !== 0) { //key q - playerOne deal if it's their turn
+    game.moveCardToMiddle(); //move playerOne top card to middle
+    game.playerTurn = 2; //change player turn
   } else if (key === 80 && game.playerTurn === 2 && game.playerTwo.hand.length !== 0) { //key p - playerTwo deal - playerTwo deal if it's their turn
-    game.moveCardToMiddle();//move playerTwo top card to middle
-    game.playerTurn = 1;//change player turn
+    game.moveCardToMiddle(); //move playerTwo top card to middle
+    game.playerTurn = 1; //change player turn
   } else {
     console.log('wrong key pressed') //invalid key (remove after testing)
   }
