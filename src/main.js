b673e971b816251f6de8d327a0b059ed79594185
JS <jeff.shep777@gmail.com>
2021-01-16 12:31:44 -0700

    Add keydown event listener and function to check for specific keys

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 var game = new Game();
 
-window.addEventListener('load', loadGame)
+window.addEventListener('load', loadGame);
+
+
 
 function loadGame() {
-  game.resetGame();//-resets central pile and repopulates fullDeck
+  game.resetGame();//resets central pile and repopulates fullDeck
   //update player wins from storage
+}
+
+document.addEventListener('keydown', function() {
+  checkKeyPress(event);
+})
+
+function checkKeyPress(event) {
+  var key = event.keyCode;
+  if(key === 70) {
+    console.log('f')//key f - playerOne slap
+  } else if(key === 74) {
+    console.log('j')//key j - playerTwo slap
+  } else if(key === 81) {
+    console.log('q')//key q - playerOne deal
+  } else if(key === 80) {
+    console.log('p')//key p - playerTwo deal
+  } else {
+    console.log('wrong key pressed')//invalid key
+  }
 }
