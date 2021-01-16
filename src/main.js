745ffc8f7485a4fb9b497f134f64f44c10729638
JS <jeff.shep777@gmail.com>
2021-01-16 12:10:09 -0700

    Add window load event listener and function to start new game

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var game = new Game();
+
+window.addEventListener('load', loadGame)
+
+function loadGame() {
+  game.resetGame();//-resets central pile and repopulates fullDeck
+  //update player wins from storage
+}
