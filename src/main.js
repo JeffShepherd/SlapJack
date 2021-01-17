9c49e110b94fa96bd8cd9735bbc06844676f0310
JS <jeff.shep777@gmail.com>
2021-01-17 15:49:59 -0700

    Add selector for header text and function to reassign innertext

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var game = new Game();
 var playerOnePile = document.querySelector('#playerOnePile');
 var centralPile = document.querySelector('#centralPile');
 var playerTwoPile = document.querySelector('#playerTwoPile');
-
+var headerMessage = document.querySelector('#headerMessage')
 
 
 //event listeners
@@ -18,12 +18,12 @@ function checkKeyPress(event) {
   var key = event.keyCode;
   var player;
 
-  if(key === 70) {
+  if (key === 70) {
     player = 1;
-    checkForSlapKey(player);//key f - playerOne slap
-  } else if(key === 74) {
+    checkForSlapKey(player); //key f - playerOne slap
+  } else if (key === 74) {
     player = 2;
-    checkForSlapKey(player);//key j - playerTwo slap
+    checkForSlapKey(player); //key j - playerTwo slap
   }
 
   checkForDealKey(key);
@@ -39,7 +39,7 @@ function checkForSlapKey(player) {
     }
   } else {
     if (game.slap()) {
-      checkForJackSlapAtEnd(player);//check if jack slap and other play loses
+      checkForJackSlapAtEnd(player); //check if jack slap and other play loses
       game.winCentralPile(player); //take middle cards and shuffle them into deck
     } else {
       game.punishBadSlap(player); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
@@ -49,13 +49,13 @@ function checkForSlapKey(player) {
 
 
 function checkForJackSlapAtEnd(player) {
-   if (player === 1 && game.centralPile[0].includes('jack') && getHandLength(2) === 0) {
-     console.log('game over player One wins'); //add function here that ends game
-     return
-   } else if (player === 2 && game.centralPile[0].includes('jack') && getHandLength(1) === 0) {
-     console.log('game over player Two wins')//add function here that ends game
-     return
-   }
+  if (player === 1 && game.centralPile[0].includes('jack') && getHandLength(2) === 0) {
+    console.log('game over player One wins'); //add function here that ends game
+    return
+  } else if (player === 2 && game.centralPile[0].includes('jack') && getHandLength(1) === 0) {
+    console.log('game over player Two wins') //add function here that ends game
+    return
+  }
 }
 
 
@@ -64,26 +64,26 @@ function checkForDealKey(key) {
   if (key === 81 && game.playerTurn === 1 && getHandLength(1) > 0) { //key q - playerOne deal if it's their turn
     game.moveCardToMiddle(); //move playerOne top card to middle
     game.playerTurn = 2; //change player turn
-    giveWinningPlayerDeckBack(1);//give deck back to player if they are winning and out of cards
+    giveWinningPlayerDeckBack(1); //give deck back to player if they are winning and out of cards
   } else if (key === 80 && game.playerTurn === 2 && getHandLength(2) > 0) { //key p - playerTwo deal - playerTwo deal if it's their turn
     game.moveCardToMiddle(); //move playerTwo top card to middle
     game.playerTurn = 1; //change player turn
-    giveWinningPlayerDeckBack(2);//give deck back to player if they are winning and out of cards
-  } else if(key === 81 || key === 80){
+    giveWinningPlayerDeckBack(2); //give deck back to player if they are winning and out of cards
+  } else if (key === 81 || key === 80) {
     console.log('it is not your turn') //invalid key (remove after testing)
   }
   checkForEndScenario()
 }
 
-function giveWinningPlayerDeckBack(player) {//give deck back to player if they are winning and out of cards
-  if(getHandLength(1) !== 0 || getHandLength(2) !== 0) {
+function giveWinningPlayerDeckBack(player) { //give deck back to player if they are winning and out of cards
+  if (getHandLength(1) !== 0 || getHandLength(2) !== 0) {
     return;
   }
 
-  if(player === 1) {
-    winCentralPile(1);//shuffle middle deck and give to player1 if player2 has no
-  } else if(player === 2) {
-    winCentralPile(2);//shuffle middle deck and give to player
+  if (player === 1) {
+    winCentralPile(1); //shuffle middle deck and give to player1 if player2 has no
+  } else if (player === 2) {
+    winCentralPile(2); //shuffle middle deck and give to player
   }
 }
 
@@ -96,25 +96,25 @@ function checkForEndScenario() {
 }
 
 function getHandLength(player) {
-  if(player === 1) {
+  if (player === 1) {
     return game.playerOne.hand.length
-  } else if(player === 2) {
+  } else if (player === 2) {
     return game.playerTwo.hand.length
   }
 }
 
 function loadGame() {
   game.resetGame(); //resets central pile and repopulates fullDeck
+  refreshCardImages(); //update card images on DOM
   //update player wins from storage
 }
 
 
 //DOM manipulating functions
 function refreshCardImages() {
-  updatePlayerPile(1, playerOnePile);
-  updatePlayerPile(2, playerTwoPile);
-
-  if(game.centralPile.length === 0) {
+  updatePlayerPileImage(1, playerOnePile);
+  updatePlayerPileImage(2, playerTwoPile);
+  if (game.centralPile.length === 0) {
     addClass(centralPile);
   } else {
     removeClass(centralPile);
@@ -122,12 +122,20 @@ function refreshCardImages() {
   }
 }
 
-function updatePlayerPile(player, element) {
-  if(getHandLength(player) === 0) {
+function updatePlayerPileImage(player, element) {
+  if (getHandLength(player) === 0) {
     addClass(element);
   } else {
     removeClass(element);
+  }
 }
+
+function displayHeaderMessage(message) {
+  removeClass(headerMessage);
+  headerMessage.innerText = message;
+  setTimeout(function() {
+    addClass(headerMessage);
+  }, 7000)
 }
 
 function addClass(element, className) {
@@ -137,9 +145,3 @@ function addClass(element, className) {
 function removeClass(element, className) {
   element.classList.remove(className || "hidden");
 }
-
-
-
-// var playerOnePile = document.querySelector('#playerOnePile');
-// var centralPile = document.querySelector('#centralPile');
-// var playerTwoPile = document.querySelector('#playerTwoPile');
