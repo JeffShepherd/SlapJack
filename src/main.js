5c5a90f9fd36174a9930887c2d113c5be84d4bae
JS <jeff.shep777@gmail.com>
2021-01-17 09:31:08 -0700

    Refactor checkForSlapKey function to be more dynamic and readable

--- src/main.js
+++ src/main.js
@@ -1,60 +1,49 @@
 var game = new Game();
 
 window.addEventListener('load', loadGame);
-
-
-
-function loadGame() {
-  game.resetGame(); //resets central pile and repopulates fullDeck
-  //update player wins from storage
-}
-
 document.addEventListener('keydown', function(event) {
   checkKeyPress(event);
 })
 
 function checkKeyPress(event) {
   var key = event.keyCode;
-  checkForSlapKey(key);
+  var player;
+
+  if(key === 70) {
+    player = 1;
+    checkForSlapKey(player);
+  } else if(key === 74) {
+    player = 2;
+    checkForSlapKey(player);
+  }
+
   checkForDealKey(key);
 }
 
-function checkForSlapKey(key) {
 
-
-  if (key === 70 && getHandLength(1) === 0) {//key f - playerOne slap
+function checkForSlapKey(player) {
+  if (getHandLength(player) === 0) {//key f - playerOne slap
     if (game.slapAtEndGame()) {
-      game.winCentralPile(1)
+      game.winCentralPile(player)
     } else {
       console.log('game over') //add function here that ends game
     }
-  } else if (key === 70) {//key f - playerOne slap
+  } else {//key f - playerOne slap
     if (game.slap()) {
-      game.winCentralPile(1); //take middle cards and shuffle them into deck
-      checkForJackSlapAtEnd(1);//check if jack slap and other play loses
+      game.winCentralPile(player); //take middle cards and shuffle them into deck
+      checkForJackSlapAtEnd(player);//check if jack slap and other play loses
     } else {
-      game.punishBadSlap(1); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
-    }
-  } else if (key === 74 && getHandLength(2) === 0) { //key j - playerTwo slap
-    if (game.slapAtEndGame()) {
-      game.winCentralPile(2)
-    } else {
-      console.log('game over') //add function here that ends game
-    }
-  } else if (key === 74) {//key j - playerTwo slap
-    if (game.slap()) {
-      game.winCentralPile(2); //take middle cards and shuffle them into deck
-      checkForJackSlapAtEnd(2);//check if jack slap and other play loses
-    } else {
-      game.punishBadSlap(2); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
+      game.punishBadSlap(player); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
     }
   }
 }
+
 
 // function checkForEndScenario()
 ////check for end conditions ( both players have 0 and winning player slaps jack and gets full shuffled deck)
 //other condition: solved: if it's a good slap of jack and other player has 0 cards: game win
 //other condition: solved: bad slap by losing player is game win
+//other condition:
 //losing player can only slap jack
 //pass in player
 
@@ -96,4 +85,9 @@ getHandLength(player) {
   } else if(player === 2) {
     return game.playerTwo.hand.length
   }
+}
+
+function loadGame() {
+  game.resetGame(); //resets central pile and repopulates fullDeck
+  //update player wins from storage
 }
