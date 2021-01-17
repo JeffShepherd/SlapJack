90c24e4040d8b2911cf926ce9281b33703fa21dc
JS <jeff.shep777@gmail.com>
2021-01-17 08:47:25 -0700

    Add function to control turn at end game

--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ function checkForSlapKey(key) {
   } else if (key === 70) {//key f - playerOne slap
     if (game.slap()) {
       game.winCentralPile(1); //take middle cards and shuffle them into deck
+      checkForJackSlapAtEnd(1);//check if jack slap and other play loses
     } else {
       game.punishBadSlap(1); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
     }
@@ -41,6 +42,7 @@ function checkForSlapKey(key) {
   } else if (key === 74) {//key j - playerTwo slap
     if (game.slap()) {
       game.winCentralPile(2); //take middle cards and shuffle them into deck
+      checkForJackSlapAtEnd(2);//check if jack slap and other play loses
     } else {
       game.punishBadSlap(2); //bad slap (slapper loses top card of their hand and it goes to bottom of opponent's hand)
     }
@@ -49,20 +51,22 @@ function checkForSlapKey(key) {
 
 // function checkForEndScenario()
 ////check for end conditions ( both players have 0 and winning player slaps jack and gets full shuffled deck)
-//other condition: if it's a good slap of jack and other player has 0 cards: game win
-//other condition: bad slap by losing player is game win
+//other condition: solved: if it's a good slap of jack and other player has 0 cards: game win
+//other condition: solved: bad slap by losing player is game win
 //losing player can only slap jack
 //pass in player
-function checkForEndScenario() {
-  if (game.playerOne.hand.length === 0) {
-    game.playerTurn = 2;
-  } else if (game.playerTwo.hand.length === 0) {
-    game.playerTurn = 1;
-  }
+
+function checkForJackSlapAtEnd(player) {
+   if (player === 1 && game.centralPile[0].includes('jack') && game.playerTwo.hand.length === 0) {
+     console.log('game over player One wins'); //add function here that ends game
+   } else if (player === 2 && game.centralPile[0].includes('jack') && game.playerOne.hand.length === 0) {
+     console.log('game over player Two wins')//add function here that ends game
+   }
 }
 
 
 function checkForDealKey(key) {
+  checkForEndScenario();
   if (key === 81 && game.playerTurn === 1 && game.playerOne.hand.length !== 0) { //key q - playerOne deal if it's their turn
     game.moveCardToMiddle(); //move playerOne top card to middle
     game.playerTurn = 2; //change player turn
@@ -71,5 +75,14 @@ function checkForDealKey(key) {
     game.playerTurn = 1; //change player turn
   } else {
     console.log('wrong key pressed') //invalid key (remove after testing)
+  }
+  checkForEndScenario()
+}
+
+function checkForEndScenario() {
+  if (game.playerOne.hand.length === 0) {
+    game.playerTurn = 2;
+  } else if (game.playerTwo.hand.length === 0) {
+    game.playerTurn = 1;
   }
 }
