aba49265418473a5919862eba3d29444f1362e02
JS <jeff.shep777@gmail.com>
2021-01-19 11:52:09 -0700

    Add missing semicolons to main.js

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ function checkKeyPress(event) {
     return displayHeaderMessage(`Invalid key pressed!`); //can remove turn and leave function call?
   }
   console.log(game.playerOne.hand, game.playerTwo.hand, game.centralPile) //remove after testing
-}
+};
 
 function assignPlayer(key) {
   if (key === 'f' || key === 'q') {
@@ -35,12 +35,12 @@ function assignPlayer(key) {
   } else if (key === 'j' || key === 'p') {
     return 2;
   }
-}
+};
 
 function checkForSlapKey(player) {
   if (getHandLength(player) === 0) { //if player's hand length is 0
     if (game.slapAtEndGame()) { //check if top card in center is a jack
-      game.winCentralPile(player) //player gets central pile
+      game.winCentralPile(player); //player gets central pile
       updateDisplayAfterTurn(`Good slap! Player${player} is back in the game!!`);
     } else {
       winGame(switchPlayer(player)); //console.log('opposing player wins, works for player 1 and 2')
@@ -58,7 +58,7 @@ function checkForSlapKey(player) {
       updateDisplayAfterTurn(`Bad slap! Player${player} forfeits a card!!`); //show header message
     }
   }
-}
+};
 
 function switchPlayer(player) {
   if (player === 1) {
@@ -66,13 +66,13 @@ function switchPlayer(player) {
   } else if (player === 2) {
     return 1;
   }
-}
+};
 
 function checkForJackSlapAtEnd(player) {
   if (game.centralPile[0].includes('jack') && getHandLength(switchPlayer(player)) === 0) {
     return true;
   }
-}
+};
 
 function checkForDealKey(key, player) {//refactor opportunity
   if (key === 'q' && game.playerTurn === 1 && getHandLength(1) > 0) { //key q - playerOne deal if it's their turn
@@ -89,8 +89,8 @@ function checkForDealKey(key, player) {//refactor opportunity
     console.log(player) //test = failed
     displayHeaderMessage(`Player${player} jumped the gun! It's not your turn!`);
   }
-  checkForEndScenario() //if one player has no cards, make sure it's the other's turn
-}
+  checkForEndScenario(); //if one player has no cards, make sure it's the other's turn
+};
 
 function giveWinningPlayerDeckBack(player) { //give deck back to player if they are winning and out of cards
   if (getHandLength(1) !== 0 || getHandLength(2) !== 0) { //if either player has cards, exit function
@@ -101,7 +101,7 @@ function giveWinningPlayerDeckBack(player) { //give deck back to player if they 
   } else if (player === 2) {
     game.winCentralPile(2); //shuffle middle deck and give to player
   }
-}
+};
 
 function checkForEndScenario() {
   if (getHandLength(1) === 0) {
@@ -109,39 +109,39 @@ function checkForEndScenario() {
   } else if (getHandLength(2) === 0) {
     game.playerTurn = 1;
   }
-}
+};
 
 function getHandLength(player) {
   if (player === 1) {
-    return game.playerOne.hand.length
+    return game.playerOne.hand.length;
   } else if (player === 2) {
-    return game.playerTwo.hand.length
+    return game.playerTwo.hand.length;
   }
-}
+};
 
 function loadGame() {
   game.resetGame(); //resets central pile and repopulates fullDeck
   refreshCardImages(); //update card images on DOM
   updateWinCountDisplay(); //reset win count display to reflect current wins
-}
+};
 
 function winGame(player) {
   game.resetGame(); //sets p1 p2 and central pile to [], then shuffles and deals full deck
   game.updateWinCount(player); //adds 1 to winning player's win count
   updateDisplayAfterTurn(`PLAYER${player} WINS!`); //refresh card images and show win message
   updateWinCountDisplay(); //reset win count display to reflect current wins
-}
+};
 
 //DOM manipulating functions
 function updateDisplayAfterTurn(message) {
   refreshCardImages();
   displayHeaderMessage(message);
-}
+};
 
 function updateWinCountDisplay() {
-  playerOneWins.innerText = `${game.playerOne.wins} Wins`
-  playerTwoWins.innerText = `${game.playerTwo.wins} Wins`
-}
+  playerOneWins.innerText = `${game.playerOne.wins} Wins`;
+  playerTwoWins.innerText = `${game.playerTwo.wins} Wins`;
+};
 
 function refreshCardImages() {
   updatePlayerPileImage(1, playerOnePile);
@@ -150,9 +150,9 @@ function refreshCardImages() {
     addClass(centralPile);
   } else {
     removeClass(centralPile);
-    centralPile.src = `assets/${game.centralPile[0]}.png`
+    centralPile.src = `assets/${game.centralPile[0]}.png`;
   }
-}
+};
 
 function updatePlayerPileImage(player, element) {
   if (getHandLength(player) === 0) {
@@ -160,7 +160,7 @@ function updatePlayerPileImage(player, element) {
   } else {
     removeClass(element);
   }
-}
+};
 
 function displayHeaderMessage(message) {
   removeClass(headerMessage);
@@ -168,12 +168,12 @@ function displayHeaderMessage(message) {
   setTimeout(function() {
     addClass(headerMessage);
   }, 7000)
-}
+};
 
 function addClass(element, className) {
   element.classList.add(className || "hidden");
-}
+};
 
 function removeClass(element, className) {
   element.classList.remove(className || "hidden");
-}
+};
